b94b6caf61247e2075ec82b8d367d0aac2637202
Ryan Talbot <ryan-talbot@outlook.com>
2021-10-28 15:27:40 +0100

    project restructure, clean up and extra notes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 import java.util.Random;
 
+/*
+This is a program to search for the word "fun" in a 1000x1000
+character array. We first instantiate a counter object then
+generate our working array. We then create 3 threads to search
+in each direction by passing the corresponding runnable.
+
+As each thread works on its task, the counter variable is
+update in a thread safe manor by using synchronized in its
+class methods which increment and return the count.
+
+Results are coming back with a word count of ~200 which is
+what we were expecting. Runtime is also calculated and shown.
+ */
+
 public class Main {
+
+    // Counter object to track word count across multiple threads
+    static Counter counter = new Counter();
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -15,18 +32,6 @@ public class Main {
             }
         }
 
-//        charArray[0][0] = 'f';
-//        charArray[1][1] = 'u';
-//        charArray[2][2] = 'n';
-//
-//        charArray[5][3] = 'f';
-//        charArray[6][4] = 'u';
-//        charArray[7][5] = 'n';
-//
-//        charArray[2][7] = 'f';
-//        charArray[3][8] = 'u';
-//        charArray[4][9] = 'n';
-
         for (int k = 0; k < charArray.length; k++) {
             System.out.printf("%-3d", k);
         }
@@ -40,28 +45,90 @@ public class Main {
         }
         // Core given code finish
 
-        SearchThread verticalSearch = new SearchThread(charArray, "vertical");
-        SearchThread horizontalSearch = new SearchThread(charArray, "horizontal");
-        SearchThread diagonalSearch = new SearchThread(charArray, "diagonal");
+        // Creating a new thread for each search direction
+        Thread verticalSearchThread = new Thread(verticalSearch(charArray));
+        Thread horizontalSearchThread = new Thread(horizontalSearch(charArray));
+        Thread diagonalSearchThread = new Thread(diagonalSearch(charArray));
 
-        Thread threadT1 = new Thread(verticalSearch);
-        Thread threadT2 = new Thread(horizontalSearch);
-        Thread threadT3 = new Thread(diagonalSearch);
-
+        // Starting to measure run time
         long start = System.currentTimeMillis();
 
-        threadT1.start();
-        threadT2.start();
-        threadT3.start();
-        threadT1.join();
-        threadT2.join();
-        threadT3.join();
+        // Starting the 3 threads
+        verticalSearchThread.start();
+        horizontalSearchThread.start();
+        diagonalSearchThread.start();
 
+        // Merging the results
+        verticalSearchThread.join();
+        horizontalSearchThread.join();
+        diagonalSearchThread.join();
+
+        // Done measuring run time
         long end = System.currentTimeMillis();
 
-        int totalCount = SearchThread.getWordCount();
+        // Getting the total word count
+        int totalCount = counter.getCount();
 
+        // Results
         System.out.println("Count: " + (totalCount));
-        System.out.println("Time: " + (end - start));
+        System.out.println("Time: " + (end - start) + "ms");
+    }
+
+    /*
+    Vertical search which iterates through each element in a column
+    and if it finds the first letter of the word in question, checks
+    the next 2. If found it increments the count.
+     */
+    private static Runnable verticalSearch(char[][] wordSearchArray) {
+        return () -> {
+            for (int row = 0; row < wordSearchArray.length; row++) {
+                for (int col = 0; col < wordSearchArray.length - 2; col++) {
+                    if (wordSearchArray[row][col] == 102 && wordSearchArray[row][col + 1] == 117 && wordSearchArray[row][col + 2] == 110) {
+                        counter.incrementAndGet();
+                    }
+                }
+            }
+        };
+    }
+
+    /*
+    Exact same as the vertical search but works on the horizontal axis
+     */
+    private static Runnable horizontalSearch(char[][] wordSearchArray) {
+        return  () -> {
+            for (int row = 0; row < wordSearchArray.length - 2; row++) {
+                for (int col = 0; col < wordSearchArray.length; col++) {
+                    if (wordSearchArray[row][col] == 102 && wordSearchArray[row + 1][col] == 117 && wordSearchArray[row + 2][col] == 110) {
+                        counter.incrementAndGet();
+                    }
+                }
+            }
+        };
+    }
+
+    /*
+    Again similar to previous searches just we search for upper and
+    lower diagonal separately.
+     */
+    private static Runnable diagonalSearch(char[][] wordSearchArray) {
+        return () -> {
+            // Upper diagonal search
+            for (int row = 2; row < wordSearchArray.length; row++) {
+                for (int col = 0; col < wordSearchArray.length - 2; col++) {
+                    if (wordSearchArray[row][col] == 102 && wordSearchArray[row - 1][col + 1] == 117 && wordSearchArray[row - 2][col + 2] == 110) {
+                        counter.incrementAndGet();
+                    }
+                }
+            }
+
+            // Lower diagonal search
+            for (int row = 0; row < wordSearchArray.length - 2; row++) {
+                for (int col = 0; col < wordSearchArray.length - 2; col++) {
+                    if (wordSearchArray[row][col] == 102 && wordSearchArray[row + 1][col + 1] == 117 && wordSearchArray[row + 2][col + 2] == 110) {
+                        counter.incrementAndGet();
+                    }
+                }
+            }
+        };
     }
 }
